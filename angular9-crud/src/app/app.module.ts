8693ae72aa951629aede68c50f6419ba42568463
Caroline STone <docphilstone@gmail.com>
2020-08-19 10:38:38 -0500

    imported HttpClientModule

--- angular9-crud/src/app/app.module.ts
+++ angular9-crud/src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ArticleComponent } from './article/article.component';
+import {ArticleService } from './article.service';
 
 @NgModule({
   declarations: [
@@ -12,9 +15,13 @@ import { ArticleComponent } from './article/article.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    ArticleService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
